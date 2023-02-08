e738945d19b94fe49b02b555fb2bb7a09e2be3f2
Anvesh800 <anvesh@whozzat.com>
2023-02-08 16:48:21 +0530

    footer

--- components/footer.js
+++ components/footer.js
@@ -1,24 +1,40 @@
 import HeadTag from "./head";
+import Image from "next/image";
 
 
 const Footer = () => {
     return (
         <>
-            <div class="container d-flex flex-direction-column align-items-center">
-                <div class="column">
-                    Column 1
-                </div>
-                <div class="column">
-                    Column 2
-                </div>
-                <div class="column">
-                    Column 3
-                </div>
-                <div class="column">
-                    Column 4
-                </div>
-                <div class="column">
-                    Column 5
+            <div className="container d-flex flex-direction-column align-items-center">
+                <div className="d-flex flex-diretion-row jutsify-content-space-between align-items-flex-start">
+                    <div className="column flex-direction-column align-items-flex-start">
+                        <div className="column flex-direction-column align-items-flex-start">
+                            <div className="text1 fs-6 ms-5">Download apps</div>
+                            <div className="ms-5"><Image src={"/img/android.svg"} alt="#" width={20} height="23"/>
+                            <Image src={"/img/apple.svg"} alt="#" width={20} height="18" className="ms-4"/>
+                            </div> 
+                        </div>
+                        <div className="column flex-direction-column align-items-flex-start mt-4">
+                        <div className="text1 fs-6 ms-5">Follow us</div>
+                            <div className="ms-5"><Image src={"/img/linkedin.svg"} alt="#" width={20} height="23"/>
+                            <Image src={"/img/facebook.svg"} alt="#" width={20} height="18" className="ms-4"/>
+                            <Image src={"/img/twitter.svg"} alt="#" width={20} height="18" className="ms-4"/>
+                            </div> 
+                        </div>
+                    </div>
+                    <div className="vl"></div>
+                    <div className="column">
+                        
+                    </div>
+                    <div className="column">
+                        
+                    </div>
+                    <div className="column">
+                        
+                    </div>
+                    <div className="column">
+                        
+                    </div>
                 </div>
             </div>
         </>
--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,14 @@ import HeadTag from "@/components/head";
 import Header from "@/components/header";
 import Link from "next/link";
 import Image from "next/image";
+import Footer from "@/components/footer";
 
 const IndexPage =()=> {
   return (
     <>
       <HeadTag/>
       <Header/>
-      
+      <Footer/>
     
     </>
 
