4e122670b1fd0e8a2347f6efa0314ad686d3149b
Anvesh800 <anvesh@whozzat.com>
2023-02-08 15:22:54 +0530

    started footer

---
+++ components/footer.js
@@ -0,0 +1,29 @@
+import HeadTag from "./head";
+
+
+const Footer = () => {
+    return (
+        <>
+            <div class="container d-flex flex-direction-column align-items-center">
+                <div class="column">
+                    Column 1
+                </div>
+                <div class="column">
+                    Column 2
+                </div>
+                <div class="column">
+                    Column 3
+                </div>
+                <div class="column">
+                    Column 4
+                </div>
+                <div class="column">
+                    Column 5
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Footer;
+
---
+++ pages/index.js
@@ -0,0 +1,42 @@
+import HeadTag from "@/components/head";
+import Footer from "@/components/footer";
+import Head from "next/head";
+import React from "react";
+const IndexPage =()=> {
+  return (
+    <>
+    <Footer/>
+    </>
+  )
+};
+export default IndexPage;
+
+// import HeadTag from "./head";
+
+// const style = {
+//     headerContainer: {
+// color: "#d3d3d3", fontSize:"24px"
+//     }
+// }
+
+// const Header = () => {
+//     return (
+//         <>
+//            <div style={style.headerContainer}>
+//             Hello
+//            </div>
+//         </>
+//     )
+// }
+
+// const Header = () => {
+//     return (
+//         <>
+//            <div style={{color: "blue", fontSize:"24px", hover: {color:"red"}}}>
+//             Hello
+//            </div>
+//         </>
+//     )
+// }
+
+// export default Header;
