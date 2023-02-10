5b5021b8d4a56bd2eca2d6755066c56011c3279f
Anvesh800 <anvesh@whozzat.com>
2023-02-10 13:23:14 +0530

    campaign

--- components/footer.js
+++ components/footer.js
@@ -5,6 +5,7 @@ import Image from "next/image";
 const Footer = () => {
     return (
         <>
+<<<<<<< Updated upstream
         <div className="container p-5">
             <div className="awesomeness row py-4 justify-content-center">
                 <div className="awesomeness_1 d-flex col-12 col-md-12 col-lg-10 me-5">
@@ -35,6 +36,62 @@ const Footer = () => {
                             <button className="button_Style1 mx-auto"><li className="mx-4">Get Started Free</li></button>
                             </ul>
                         </div>
+=======
+            <div className="footer container-fluid d-flex justify-content-center flex-direction-column align-items-center">
+                <div className="d-flex flex-diretion-row jutsify-content-space-between align-items-flex-start">
+                    <div className="column flex-direction-column align-items-flex-start">
+                        <div className="column flex-direction-column align-items-flex-start">
+                            <div className="text1 ms-5">Download apps</div>
+                            <div className="ms-5"><Image src={"/img/android.svg"} alt="#" width={20} height="23" />
+                                <Image src={"/img/apple.svg"} alt="#" width={20} height="18" className="ms-4" />
+                            </div>
+                        </div>
+                        <div className="column flex-direction-column align-items-flex-start mt-4">
+                            <div className="text1 fs-6 ms-5">Follow us</div>
+                            <div className="ms-5"><Image src={"/img/linkedin.svg"} alt="#" width={20} height="23" />
+                                <Image src={"/img/facebook.svg"} alt="#" width={20} height="18" className="ms-4" />
+                                <Image src={"/img/twitter.svg"} alt="#" width={20} height="18" className="ms-4" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="vl"></div>
+                    <div className="column flex-direction-column align-items-flex-start me-5">
+                        <div className="text2">Our Products</div>
+                        <div className="text3 mt-1">One API</div>
+                        <div className="text3 mt-1">Send OTP</div>
+                        <div className="text3 mt-1">Hello</div>
+                        <div className="text3 mt-1">Segmento</div>
+                    </div>
+                    <div className="column flex-direction-column align-items-flex-start ms-5 me-5">
+                        <div className="text2">Channels</div>
+                        <div className="text3 mt-1">SMS</div>
+                        <div className="text3 mt-1">Email</div>
+                        <div className="text3 mt-1">Voice</div>
+                        <div className="text3 mt-1">RCS</div>
+                        <div className="text3 mt-1">Virtual number</div>
+                        <div className="text3 mt-1">WhatsApp</div>
+                        <div className="text3 mt-1">View all channels</div>
+                    </div>
+                    <div className="column flex-direction-column align-items-flex-start ms-5 me-5">
+                    <div className="text2">Resources</div>
+                        <div className="text3 mt-1">Indutries</div>
+                        <div className="text3 mt-1">Blog</div>
+                        <div className="text3 mt-1">Case studies</div>
+                        <div className="text3 mt-1">FAQ</div>
+                        <div className="text3 mt-1">API doc</div>
+                    </div>
+                    <div className="column flex-direction-column align-items-flex-start ms-5">
+                    <div className="text2">Discover</div>
+                        <div className="text3 mt-1">About us</div>
+                        <div className="text3 mt-1">Pricing</div>
+                        <div className="text3 mt-1">Contact us </div>
+                        <div className="text3 mt-1">Partner program</div>
+                        <div className="text3 mt-1">MSG91 for Developers</div>
+                        <div className="text3 mt-1">MSG91 for Startups</div>
+                        <div className="text3 mt-1">Tools and Addons</div>
+                        <div className="text3 mt-1">Our partners</div>
+                        <div className="text3 mt-1">Carrers</div>
+>>>>>>> Stashed changes
                     </div>
                 </div>
 
