fcbc47720b8fef0b4d08976c25c16bf4cac8cfc7
Anvesh800 <anvesh@whozzat.com>
2023-02-20 18:04:06 +0530

    email

--- pages/email.js
+++ pages/email.js
@@ -15,18 +15,19 @@ const email = () => {
       <div className="container text-center px-4  col-12 col-sm-10  ">
         <div className="text-center justify-content-center py-5">
           <div className="d-flex justify-content-center align-items-center flex-column flex-sm-row">
-            <img src="../img/campaign.svg" className="product-page-logo" />
-            <h1 className="heading">Campaign</h1>
+            <h1 className="heading">Deliver a Consistent Experience
+              with Transactional Email</h1>
           </div>
-          <p className="c-fs-4 mx-auto">Event based automation</p>
           <h2 className="c-fs-2 c-ff-b c-fw-r w-100 mx-auto">
-            Boost audience engagement through smart communication and on-time
-            delivery.{" "}
+            With our developer friendly APIs, effortlessly
+            deliver informational transactional email that has high open rates.
+          </h2>
+          <h2 className="c-fs-2 c-ff-b c-fw-r w-100 mx-auto">
+            Keep your customers updated with their order details, booking status,login prompts, and other essential information.
           </h2>
           <button className="btn btn-primary btn-lg mt-3 c-fs-3" type="button">
-            Get started for Free
+            Get started
           </button>
-          <TrustedSec />
         </div>
         <img
           src={"/img/Campaign_flow.svg"}
@@ -34,6 +35,14 @@ const email = () => {
           alt="#"
         />
       </div>
+
+      <div className="container d-flex justify-content-center align-items-center flex-column flex-sm-row">
+        <h1 className="heading mt-5">Send data-driven transactional emails</h1>
+      </div>
+      <h2 className="container c-fs-2 c-ff-b c-fw-r w-100 mx-auto py-5">
+        Transactional emails are a unique set of one to one emails that are
+        ideally targeted for sending data-driven and personalized messages
+      </h2>
 
       <div className=" c-bg-grey px-sm-0 d-flex flex-column justify-content-center justift-content-sm-start section ">
         <span className="container c-fs-1 c-ff-h c-fw-m ">Features</span>
@@ -44,13 +53,12 @@ const email = () => {
           </div>
           <div className="col col-lg-6 col-md-6 col-sm-12 col-xs-12 d-flex flex-column align-items-flex-start">
             <span className="c-fs-2 c-ff-h cc-fw-m">
-              Automate sending messages under defined conditions
+              Email Tracking and Analytics
             </span>
             <span className="c-fs-3  mt-3">
-              Define different messaging parameters like time, region, number of
-              messages per day, and more to make sure that your campaigns reach
-              your audience under the right circumstances automatically, without
-              any delays.
+              Get access to the reporting dashboard to analyse transactional email data like open rate, click-through rate, A/B testing etc.
+              Debug a specific request or status of an email delivery or analyse report.
+              Get 30 days of log retention and 7 days message retention.
             </span>
           </div>
         </div>
@@ -61,113 +69,55 @@ const email = () => {
           </div>
           <div className="col col-lg-6 col-md-6 col-sm-12 col-xs-12 d-flex flex-column align-items-flex-start">
             <span className="c-fs-2 c-ff-h n">
-              An all-in-one platform for all your campaigns
+              Suppression Management
             </span>
             <span className="c-fs-3  mt-3">
-              Optimise your links and create short links that inspire
-              confidence. Include short links in your custom messages with ease.
-            </span>
-          </div>
-        </div>
-
-        <div className="container d-lg-flex flex-row align-items-center mt-5">
-          <div className=" col-6 flex-column align-items-flex-start order-2">
-            <img src="/img/camp-f-img-three.svg" className="feature-img" />
-          </div>
-          <div className="col col-lg-6 col-md-6 col-sm-12 col-xs-12 d-flex flex-column align-items-flex-start">
-            <span className="c-fs-2 c-ff-h ">
-              Make your communication more secure
+              Manage your opt-in, opt-out contacts and automatically deliver with our transactional email provider. Make the
+              suppression list management easy. Automate the entire email opt-out collection
+              with the webhooks to process and distribute in real-time.
             </span>
             <span className="c-fs-3  mt-3">
-              Now protect your Campaigns with our token-based workflow and a
-              robust security framework. Implement best customer-engagement
-              practices and shape communication to fit your brand while ensuring
-              optimum security through and through.
+              <strong>Automation in the opt-out collection also works for other channels like SMS.</strong>
             </span>
           </div>
         </div>
       </div>
 
-      <div className="container d-flex flex-column flex-md-row align-items-center mt-5 mb-5">
-        <div className="col-lg-4 flex-column align-items-flex-start">
-          <div className="mt-5 mb-3">
-            <img
-              src={"/Img/done_outline.svg"}
-              alt="#"
-              className="feature-check"
-            />
-          </div>
-          <span className="c-fs-2 c-ff-h  mt-1">
-            Pre-defined templates for improved productivity
-          </span>
-          <div>
-            <span className="c-fs-3 mt-2">
-              Leverage our pre-defined templates to reduce turnaround time for
-              your campaigns. Just import & deploy campaigns on the fly within
-              minutes.
-            </span>
-          </div>
+      <div class="container d-flex col col-12 py-5">
+        <div className="col-5 bg-opacity-10 border rounded-end justify-content-center align-items-center p-5 mx-auto">
+          <h2><strong>Receive email in JSON format</strong></h2>
+          <span className="py-5 fs-5">Send transactional emails with a lightweight data-interchange format and make them easily readable.    Get every email as JSON at your server via webhook. Setup MX record or forward email to our unique ID and we will send you as we receive.</span>
         </div>
-        <div className="col-lg-4 flex-column align-items-flex-start">
-          <div className="mt-5 mb-3">
-            <img
-              src={"/Img/done_outline.svg"}
-              alt="#"
-              className="feature-check"
-            />
-          </div>
-          <span className="c-fs-2 c-ff-h c-fw-m mt-1">
-            Pre-defined templates for improved productivity
-          </span>
-          <div>
-            <span className="c-fs-3 mt-2">
-              Leverage our pre-defined templates to reduce turnaround time for
-              your campaigns. Just import & deploy campaigns on the fly within
-              minutes.
-            </span>
-          </div>
-        </div>
-        <div className="col-lg-4 flex-column align-items-flex-start">
-          <div className="mt-5 mb-3">
-            <img
-              src={"/Img/done_outline.svg"}
-              alt="#"
-              className="feature-check"
-            />
-          </div>
-          <span className="c-fs-2 c-ff-h c-fw-m mt-1">
-            Pre-defined templates for improved productivity
-          </span>
-          <div>
-            <span className="c-fs-3 mt-2">
-              Leverage our pre-defined templates to reduce turnaround time for
-              your campaigns. Just import & deploy campaigns on the fly within
-              minutes.
-            </span>
-          </div>
+        <div className="col-5 bg-opacity-10 border rounded-end justify-content-center align-items-center p-5 mx-auto">
+          <h2><strong>Receive email in JSON format</strong></h2>
+          <span className="py-5 fs-5">Send transactional emails with a lightweight data-interchange format and make them easily readable.    Get every email as JSON at your server via webhook. Setup MX record or forward email to our unique ID and we will send you as we receive.</span>
         </div>
       </div>
 
-      <div className="container">
-        <span className="c-fs-2 c-fw-m c-ff-h">More features</span>
+      <div className="container py-5">
+        <span className="c-fs-2 c-fw-m c-ff-h">Why Choose MSG91
+for sending Transactional Email?</span>
         <div className="container d-flex flex-column flex-md-row my-3">
           <div className="d-flex flex-column justify-content-center align-items-flex-start col-lg-6 mt-2">
             <span className="c-fs-3">
-              - Automate sending messages under defined conditions
+              - Assured delivery with 4x delivery speeds
             </span>
             <span className="c-fs-3 mt-1">
-              - An all-in-one platform for all your campaigns
+              - 30 Days of Log history to help you regulate
             </span>
             <span className="c-fs-3 mt-1">
-              - Make your communication more secure
+              - Transparent delivery tracking on every email
             </span>
             <span className="c-fs-3 mt-1">
-              - Pre-defined templates for improved productivity
+              - Easy and scalable developer-friendly API to integrate
             </span>
             <span className="c-fs-3 mt-1">
-              - Build a hassle-free communication flow
+              - 18/7 Ticket support
             </span>
-            <span className="c-fs-3 mt-1">- Track campaign performance</span>
+            <span className="c-fs-3 mt-1">- Provides feature of send-time optimization for real-time</span>
+            <span className="c-fs-3 mt-1">
+            &nbsp;&nbsp;delivery
+            </span>
           </div>
           <div className="my-auto mt-3 px-5 d-flex py-5 c-bg-grey flex-wrap justify-content-center">
             <span className="c-fs-2 c-ff-h">Looking for more?</span>
