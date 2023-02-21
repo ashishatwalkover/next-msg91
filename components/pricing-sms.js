ba659f8edc8f5e3a0d476eae883353ef508adebd
piyussssh <mvps1862@gmail.com>
2023-02-21 16:12:43 +0530

    pricing/sms

--- components/pricing-sms.js
+++ components/pricing-sms.js
@@ -1,99 +1,100 @@
 import { MdDone, MdClose } from "react-icons/md";
-const pricingsms = () => {
+const pricingemail = () => {
   return (
     <>
-    <div className="d-flex card-container align-items-end">
-              <div className="card w-25 border-0 text-center me-3 c-bg-grey">
-                <div className="card-body">
-                  <h3 className="c-fs-3">Free</h3>
-                  <h5 className="c-fs-4 mt-2">$0/Month</h5>
-                  <p className="c-fs-5"> - </p>
-                  <div className="c-fs-5 mt-2">
-                    <span className="text-success c-fs-3">
-                      <MdDone />
-                    </span>
-                    100,000 Emails
-                  </div>
-                  <div className="c-fs-5 ">
-                    <span className="text-danger c-fs-3">
-                      <MdClose />
-                    </span>
-                    Email Varifications
-                  </div>
-
-                  <button className="c-fs-5 btn btn-sm w-100 btn-outline-primary mt-2">
-                    Get Started
-                  </button>
-                </div>
-              </div>
-              <div className="card w-25 border-0 text-center mx-3 c-bg-grey">
-                <div className="card-body">
-                  <h3 className="c-fs-3">Basic</h3>
-                  <h5 className="c-fs-4 mt-2">$500/Month</h5>
-                  <p className="c-fs-5">+18%GST </p>
-                  <div className="c-fs-5 mt-2">
-                    <span className="text-success c-fs-3">
-                      <MdDone />
-                    </span>
-                    250,000 Emails
-                  </div>
-                  <div className="c-fs-5 ">
-                    <span className="text-success c-fs-3">
-                      <MdDone />
-                    </span>
-                    3,000 Email Varifications
-                  </div>
-
-                  <button className="c-fs-5 btn btn-sm w-100 btn-outline-primary mt-2">
-                    Get Started
-                  </button>
-                </div>
-              </div>
-              <div className="  text-center d-flex flex-column align-items-center mx-3 w-25">
-                <div className="popular-chip c-fs-6">POPULAR</div>
-              <div className="card  w-100 card-popular border-primary text-center c-bg-grey">
-                <div className="card-body">
-                  <h3 className="c-fs-3">Alpha</h3>
-                  <h5 className="c-fs-4 mt-2">$1500/Month</h5>
-                  <p className="c-fs-5">+18%GST </p>
-                  <div className="c-fs-5 mt-2">
-                    <span className="text-success c-fs-3">
-                      <MdDone />
-                    </span>
-                    5,00,000 Emails
-                  </div>
-                  <div className="c-fs-5 ">
-                    <span className="text-success c-fs-3">
-                      <MdDone />
-                    </span>
-                    5,000 Email Varifications
-                  </div>
-
-                  <button className="c-fs-5 btn btn-sm w-100 btn-primary mt-2">
-                    Get Started
-                  </button>
-                </div>
-              </div>
-            </div>
-            <div className="card w-25 border-0 text-center ms-3 c-bg-grey">
-                <div className="card-body">
-                  <h3 className="c-fs-3">Custom</h3>
-                  <h5 className="c-fs-4 mt-2">Custom Pricing</h5>
-                  <p className="c-fs-5">-</p>
-                  <div className="c-fs-5 mt-2">
-                    Need to send more? Talk to us for a customized plan.
-                  </div>
-                 
-
-                  <button className="c-fs-5 btn btn-sm w-100 btn-outline-primary mt-2">
-                    Talk to an Expert
-                  </button>
-                </div>
-              </div>
-              </div>
-   
+      <div className="d-flex flex-wrap flex-gap justify-content-center w-100  card-container align-items-end">
+        <div className="card  border-0 text-center mb-4 mb-sm-0 c-bg-grey">
+          <div className="card-body">
+            <h3 className="c-fs-3">5,000 SMS</h3>
+            <h5 className="c-fs-4 mt-2">₹0.25/SMS</h5>
+            <p className="c-fs-5"> - </p>
+            <h2 className="c-fs-2 c-ff-b text-green">₹1,259 </h2>
+            <p className="c-fs-5">+18% GST</p>
+            <button className="c-fs-5 btn btn-sm w-100 btn-outline-primary mt-2">
+              Get Started
+            </button>
+          </div>
+        </div>
+        <div className="card  border-0 text-center mb-4 mb-sm-0 c-bg-grey">
+          <div className="card-body">
+            <h3 className="c-fs-3">20,000 SMS</h3>
+            <h5 className="c-fs-4 mt-2">₹0.20/SMS</h5>
+            <p className="c-fs-5"> - </p>
+            <h2 className="c-fs-2 c-ff-b text-green">₹4,000</h2>
+            <p className="c-fs-5">+18% GST</p>
+            <button className="c-fs-5 btn btn-sm w-100 btn-outline-primary mt-2">
+              Get Started
+            </button>
+          </div>
+        </div>
+     
+        <div className="  mx-3 text-center d-flex flex-column mb-4 mb-sm-0 align-items-center ">
+          <div className="popular-chip c-fs-6">POPULAR</div>
+          <div className="card  text-center card-popular border-primary mb-4 mb-sm-0 c-bg-grey">
+          <div className="card-body">
+            <h3 className="c-fs-3">50,000 SMS</h3>
+            <h5 className="c-fs-4 mt-2">₹0.18/SMS</h5>
+            <p className="c-fs-5"> - </p>
+            <h2 className="c-fs-2 c-ff-b text-green">₹9,000 </h2>
+            <p className="c-fs-5">+18% GST</p>
+            <button className="c-fs-5 btn btn-sm w-100 btn-primary mt-2">
+              Get Started
+            </button>
+          </div>
+        </div>
+        </div>
+        <div className="card  border-0 text-center mb-4 mb-sm-0 c-bg-grey">
+          <div className="card-body">
+            <h3 className="c-fs-3">1,00,000 SMS</h3>
+            <h5 className="c-fs-4 mt-2">₹.17/SMS</h5>
+            <p className="c-fs-5"> - </p>
+            <h2 className="c-fs-2 c-ff-b text-green">₹17,000</h2>
+            <p className="c-fs-5">+18% GST</p>
+            <button className="c-fs-5 btn btn-sm w-100 btn-outline-primary mt-2">
+              Get Started
+            </button>
+          </div>
+        </div>
+        <div className="card  border-0 text-center mb-4 mb-sm-0 c-bg-grey">
+          <div className="card-body">
+            <h3 className="c-fs-3">3,00,000 SMS</h3>
+            <h5 className="c-fs-4 mt-2">₹.14/SMS</h5>
+            <p className="c-fs-5"> - </p>
+            <h2 className="c-fs-2 c-ff-b text-green">₹48,000</h2>
+            <p className="c-fs-5">+18% GST</p>
+            <button className="c-fs-5 btn btn-sm w-100 btn-outline-primary mt-2">
+              Get Started
+            </button>
+          </div>
+        </div>
+        <div className="card  border-0 text-center mb-4 mb-sm-0 c-bg-grey">
+          <div className="card-body">
+            <h3 className="c-fs-3">5,00,000 SMS</h3>
+            <h5 className="c-fs-4 mt-2">₹.12/SMS</h5>
+            <p className="c-fs-5"> - </p>
+            <h2 className="c-fs-2 c-ff-b text-green">₹75,000</h2>
+            <p className="c-fs-5">+18% GST</p>
+            <button className="c-fs-5 btn btn-sm w-100 btn-outline-primary mt-2">
+              Get Started
+            </button>
+          </div>
+        </div>
+        <div className="card  border-0 text-center mb-4 mb-sm-0 c-bg-grey">
+          <div className="card-body">
+            <h3 className="c-fs-3">5,00,000+ SMS</h3>
+            <h5 className="c-fs-4 mt-2">₹.10/SMS</h5>
+            <p className="c-fs-5"> - </p>
+            {/* <h2 className="c-fs-2 c-ff-b text-green">Cutomised Plan</h2> */}
+            <p className="c-fs-5">Customised Plan</p>
+            <button className="c-fs-5 btn btn-sm w-100 btn-outline-primary mt-2">
+              Talk to Sales
+            </button>
+          </div>
+        </div>
+     
+      </div>
     </>
   );
 };
 
-export default pricingsms;
+export default pricingemail;
