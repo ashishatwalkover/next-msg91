dea5be7aa48851a2bb0319d59d2fa4b8651ac287
piyussssh <mvps1862@gmail.com>
2023-02-21 17:52:18 +0530

    whastApp & voice modified

--- components/pricing-voice.js
+++ components/pricing-voice.js
@@ -1,5 +1,5 @@
 import { MdDone, MdClose } from "react-icons/md";
-const pricingsms = () => {
+const pricingCalls = () => {
   return (
     <>
       <div className="g-3 d-flex justify-content-center col-lg-5 m-auto pb-5">
@@ -14,8 +14,8 @@ const pricingsms = () => {
       <div className="d-flex flex-wrap flex-gap justify-content-center w-100  card-container align-items-end">
         <div className="card  border-0 text-center mb-4 mb-sm-0 c-bg-grey">
           <div className="card-body">
-            <h3 className="c-fs-3">5,000 SMS</h3>
-            <h5 className="c-fs-4 mt-2">₹0.25/SMS</h5>
+            <h3 className="c-fs-3">5,000 Calls</h3>
+            <h5 className="c-fs-4 mt-2">₹0.25/Calls</h5>
             <p className="c-fs-5"> - </p>
             <h2 className="c-fs-2 c-ff-b text-green">₹1,259 </h2>
             <p className="c-fs-5">+18% GST</p>
@@ -26,8 +26,8 @@ const pricingsms = () => {
         </div>
         <div className="card  border-0 text-center mb-4 mb-sm-0 c-bg-grey">
           <div className="card-body">
-            <h3 className="c-fs-3">20,000 SMS</h3>
-            <h5 className="c-fs-4 mt-2">₹0.20/SMS</h5>
+            <h3 className="c-fs-3">20,000 Calls</h3>
+            <h5 className="c-fs-4 mt-2">₹0.20/Calls</h5>
             <p className="c-fs-5"> - </p>
             <h2 className="c-fs-2 c-ff-b text-green">₹4,000</h2>
             <p className="c-fs-5">+18% GST</p>
@@ -41,8 +41,8 @@ const pricingsms = () => {
           <div className="popular-chip c-fs-6">POPULAR</div>
           <div className="card  text-center card-popular border-primary mb-4 mb-sm-0 c-bg-grey">
             <div className="card-body">
-              <h3 className="c-fs-3">50,000 SMS</h3>
-              <h5 className="c-fs-4 mt-2">₹0.18/SMS</h5>
+              <h3 className="c-fs-3">50,000 Calls</h3>
+              <h5 className="c-fs-4 mt-2">₹0.18/Calls</h5>
               <p className="c-fs-5"> - </p>
               <h2 className="c-fs-2 c-ff-b text-green">₹9,000 </h2>
               <p className="c-fs-5">+18% GST</p>
@@ -54,8 +54,8 @@ const pricingsms = () => {
         </div>
         <div className="card  border-0 text-center mb-4 mb-sm-0 c-bg-grey">
           <div className="card-body">
-            <h3 className="c-fs-3">1,00,000 SMS</h3>
-            <h5 className="c-fs-4 mt-2">₹.17/SMS</h5>
+            <h3 className="c-fs-3">1,00,000 Calls</h3>
+            <h5 className="c-fs-4 mt-2">₹.17/Calls</h5>
             <p className="c-fs-5"> - </p>
             <h2 className="c-fs-2 c-ff-b text-green">₹17,000</h2>
             <p className="c-fs-5">+18% GST</p>
@@ -66,8 +66,8 @@ const pricingsms = () => {
         </div>
         <div className="card  border-0 text-center mb-4 mb-sm-0 c-bg-grey">
           <div className="card-body">
-            <h3 className="c-fs-3">3,00,000 SMS</h3>
-            <h5 className="c-fs-4 mt-2">₹.14/SMS</h5>
+            <h3 className="c-fs-3">3,00,000 Calls</h3>
+            <h5 className="c-fs-4 mt-2">₹.14/Calls</h5>
             <p className="c-fs-5"> - </p>
             <h2 className="c-fs-2 c-ff-b text-green">₹48,000</h2>
             <p className="c-fs-5">+18% GST</p>
@@ -78,8 +78,8 @@ const pricingsms = () => {
         </div>
         <div className="card  border-0 text-center mb-4 mb-sm-0 c-bg-grey">
           <div className="card-body">
-            <h3 className="c-fs-3">5,00,000 SMS</h3>
-            <h5 className="c-fs-4 mt-2">₹.12/SMS</h5>
+            <h3 className="c-fs-3">5,00,000 Calls</h3>
+            <h5 className="c-fs-4 mt-2">₹.12/Calls</h5>
             <p className="c-fs-5"> - </p>
             <h2 className="c-fs-2 c-ff-b text-green">₹75,000</h2>
             <p className="c-fs-5">+18% GST</p>
@@ -90,8 +90,8 @@ const pricingsms = () => {
         </div>
         <div className="card  border-0 text-center mb-4 mb-sm-0 c-bg-grey">
           <div className="card-body">
-            <h3 className="c-fs-3">5,00,000+ SMS</h3>
-            <h5 className="c-fs-4 mt-2">₹.10/SMS</h5>
+            <h3 className="c-fs-3">5,00,000+ Calls</h3>
+            <h5 className="c-fs-4 mt-2">₹.10/Calls</h5>
             <p className="c-fs-5"> - </p>
             {/* <h2 className="c-fs-2 c-ff-b text-green">Cutomised Plan</h2> */}
             <p className="c-fs-5">Customised Plan</p>
@@ -105,4 +105,4 @@ const pricingsms = () => {
   );
 };
 
-export default pricingsms;
+export default pricingCalls;
