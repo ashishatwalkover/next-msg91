c1e3ff53877ead1860b461a032bb0c636461c8da
Ashish Yadav <ashish@walkover.in>
2023-02-22 18:40:59 +0530

    font variasion

--- components/head.js
+++ components/head.js
@@ -1,13 +1,30 @@
 import Head from "next/head"
+import { useRouter } from 'next/router'
 const HeadTag =()=> {
+  const router = useRouter()
+  var path = router.pathname.split("/")[1];
   return (
     <>
   <Head>
     <meta name="viewport" content="width=device-width, initial-scale=1"/>    
     <link rel="icon" href="/fav.svg"/>
+    { path === '' ?
+      <>
+        <link href="/styles/global.css" rel="stylesheet"/>
+      </>
+      :''
+    }    
+    { path === 'in' ?
+      <>
+        <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet"/>
+        <link href="/styles/in.css" rel="stylesheet"/>
+      </>
+      :''
+    }    
   </Head>
     
     </>
   )
 };
 export default HeadTag;
+//in, ph, sg, es, ae, uk, us
