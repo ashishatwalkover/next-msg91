1026fd84338009edd06a0f4309f36afa480c37a6
Ashish Yadav <ashish@walkover.in>
2023-02-23 18:29:18 +0530

    utilities scroll highlight

--- components/home.js
+++ components/home.js
@@ -1,42 +1,23 @@
 import { useEffect, useRef } from "react";
+import $ from 'jquery';
 
 const Home = () => {
   const aboutRef = useRef();
 
   useEffect(() => {
-    const shortner = document.getElementById("url-shortner");
-    const knowledge = document.getElementById("knowledge-base");
-    const file = document.getElementById("file-hosting");
-    
-    const utilitiesItems = document.getElementById("utilities-items");
-    const linkShortner = document.getElementById("link-shortner");
-    const linkKnowledge = document.getElementById("link-knowledge");
-    const linkFile = document.getElementById("link-file");
-
     const handleScroll = (e) => {      
-      if (
-        utilitiesItems.offsetTop >= shortner.offsetTop &&
-        utilitiesItems.offsetTop + 45 <= shortner.offsetTop + 495
-      ) {
-        linkShortner.classList.add("active");        
-      } else {
-        linkShortner.classList.remove("active");        
+      console.log($("#link-knowledge").offset().top , $("#knowledge-base").offset().top);
+      if ( $("#link-shortner").offset().top >= $("#url-shortner").offset().top) {
+        $(".us-nav-options").removeClass("active");
+        $("#link-shortner").addClass("active");
       }
-      if (
-        utilitiesItems.offsetTop >= knowledge.offsetTop - 45 &&
-        utilitiesItems.offsetTop + 90 <= knowledge.offsetTop + 495
-      ) {
-        linkKnowledge.classList.add("active");
-      } else {
-        linkKnowledge.classList.remove("active");
+      if ( $("#link-knowledge").offset().top >= $("#knowledge-base").offset().top) {
+        $(".us-nav-options").removeClass("active");
+        $("#link-knowledge").addClass("active");
       }
-      if (
-        utilitiesItems.offsetTop >= file.offsetTop - 90 &&
-        utilitiesItems.offsetTop <= file.offsetTop + 495
-      ) {
-        linkFile.classList.add("active");
-      } else {
-        linkFile.classList.remove("active");
+      if ( $("#link-file").offset().top >= $("#file-hosting").offset().top) {
+        $(".us-nav-options").removeClass("active");
+        $("#link-file").addClass("active");
       }
     };
     document.addEventListener("scroll", handleScroll);
