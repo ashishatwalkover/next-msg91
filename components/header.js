eef45b18a234a7e024e8a5cad3ef7c78c0553fb1
piyussssh <mvps1862@gmail.com>
2023-02-23 14:36:53 +0530

    font size 1440p added

--- components/header.js
+++ components/header.js
@@ -34,25 +34,25 @@ const Header = () => {
   var home = (path.length) ? path : '/';
   return (
     <div className="px-0 px-xl-5" id="menu-wrp">
-      <nav className="navbar px-0 px-xl-4 navbar-expand-lg bg-body-tertiary c-fs-4">
+      <nav className="navbar px-0 px-xl-4 navbar-expand-lg bg-body-tertiary c-fs-3">
         <div className="container-fluid">          
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon" />
           </button>
           <a className="navbar-brand m-auto d-lg-none d-block" href="#"><img src="/img/logo.svg" alt="msg91" /></a>
-          <button type="button" className="btn btn-primary c-fs-4 d-lg-none d-block">Sign UP</button>
+          <button type="button" className="btn btn-primary c-fs-3 d-lg-none d-block">Sign UP</button>
           <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div className="navbar-nav navbar-w product-navbar">
-              <a className="nav-link c-fs-4 c-fw-r" href="#" id="link-products">Products <span className="sr-only"></span></a>
-              <a className="nav-link c-fs-4 c-fw-r" href="#" id="link-learning">Learning Center</a>
-              <Link href="/pricing" className="nav-link c-fs-4 c-fw-r" id="link-pricing">Pricing</Link>
+              <a className="nav-link c-fs-3 c-fw-r" href="#" id="link-products">Products <span className="sr-only"></span></a>
+              <a className="nav-link c-fs-3 c-fw-r" href="#" id="link-learning">Learning Center</a>
+              <Link href="/pricing" className="nav-link c-fs-3 c-fw-r" id="link-pricing">Pricing</Link>
             </div>
             
             <Link className="navbar-brand m-auto" href={`${home}`}><img className=" d-lg-block d-none" src="/img/logo.svg" alt="MSG91" /></Link>
             
             <div className="navbar-nav justify-content-end navbar-w product-navbar d-none d-lg-flex">
-              <a className="nav-link c-fs-4 c-fw-r" href="#products">API Documentations</a>            
-              <button className=" btn  c-fs-4 btn-primary " href="#products">Sign Up</button>                          
+              <a className="nav-link c-fs-3 c-fw-r" href="#products">API Documentations</a>            
+              <button className=" btn  c-fs-3 btn-primary " href="#products">Sign Up</button>                          
             </div>
           </div>                    
         </div>
--- components/notification.js
+++ components/notification.js
@@ -25,24 +25,24 @@ const Notification = () => {
     <>
       <div className="px-0 px-md-5 c-bg-grey c-fs-4">
         <div className="px-3 px-md-5 py-1 d-flex flex-md-row flex-column align-items-start   align-items-md-center  justify-content-md-between justify-content-start">
-          <p className="">
+          <p className="c-fs-4">
             The New and Improved Version Awaits :)
           </p>
           <div className=" text-dark d-flex  ">
             <div className="d-flex  align-items-center">              
               <div className="dropdown" id="change-country">
                 <button className="btn btn-link dropdown-toggle btn-sm text-dark" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  <MdLanguage /> <label>{country}</label>
+                  <MdLanguage /> <label className="c-fs-4">{country}</label>
                 </button>
                 <ul className="dropdown-menu">
-                  <li><Link href="/" className="dropdown-item">Global</Link></li>
-                  <li><Link href="/in" className="dropdown-item">India</Link></li>
-                  <li><Link href="/ph" className="dropdown-item">Philippines</Link></li>
-                  <li><Link href="/sg" className="dropdown-item">Singapore</Link></li>
-                  <li><Link href="/es" className="dropdown-item">Spain</Link></li>
-                  <li><Link href="/ae" className="dropdown-item">United Arab Emirates</Link></li>
-                  <li><Link href="/uk" className="dropdown-item">United Kingdom</Link></li>
-                  <li><Link href="/us" className="dropdown-item">United States</Link></li>
+                  <li><Link href="/" className="dropdown-item c-fs-4">Global</Link></li>
+                  <li><Link href="/in" className="dropdown-item c-fs-4">India</Link></li>
+                  <li><Link href="/ph" className="dropdown-item c-fs-4">Philippines</Link></li>
+                  <li><Link href="/sg" className="dropdown-item c-fs-4">Singapore</Link></li>
+                  <li><Link href="/es" className="dropdown-item c-fs-4">Spain</Link></li>
+                  <li><Link href="/ae" className="dropdown-item c-fs-4">United Arab Emirates</Link></li>
+                  <li><Link href="/uk" className="dropdown-item c-fs-4">United Kingdom</Link></li>
+                  <li><Link href="/us" className="dropdown-item c-fs-4">United States</Link></li>
                   
                 </ul>
               </div>
