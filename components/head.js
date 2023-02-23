21412fe05a9e0b36947ef7fce2dca051bb81bd6c
piyussssh <mvps1862@gmail.com>
2023-02-23 16:10:42 +0530

    jost font fixed

--- components/head.js
+++ components/head.js
@@ -16,7 +16,7 @@ const HeadTag =()=> {
     }    
     { path === 'in' ?
       <>
-        <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet"/>
+        <link href="https://fonts.googleapis.com/css2?family=Jost:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet"/>
         <link href="/styles/in.css" rel="stylesheet"/>
       </>
       :''
