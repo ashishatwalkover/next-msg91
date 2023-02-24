10871e527551af14e605773698dc26a8357f223b
piyussssh <mvps1862@gmail.com>
2023-02-24 14:57:23 +0530

    minor header chnages

--- components/header.js
+++ components/header.js
@@ -35,7 +35,7 @@ const Header = () => {
   return (
     <div className="px-0  px-md-5" id="menu-wrp">
       <nav className="navbar px-0 px-xl-5 navbar-expand-lg bg-body-tertiary c-fs-3">
-        <div className=" w-100 px-3">      
+        <div className=" w-100 ">      
         <div className="d-flex w-100 justify-content-between">    
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon" />
@@ -45,9 +45,9 @@ const Header = () => {
           </div>
           <div className="collapse w-100 navbar-collapse" id="navbarNavAltMarkup">
             <div className="navbar-nav px-3 product-navbar">
-              <a className="nav-link c-fs-3 c-fw-r " href="#" id="link-products">Products <span className="sr-only"></span></a>
-              <a className="nav-link c-fs-3 c-fw-r" href="#" id="link-learning">Learning Center</a>
-              <Link href="/pricing" className="nav-link c-fs-3 c-fw-r" id="link-pricing">Pricing</Link>
+              <a className="nav-link c-fs-3 c-fw-r d-lg-block d-none" href="#" id="link-products">Products <span className="sr-only"></span></a>
+              <a className="nav-link c-fs-3 c-fw-r d-lg-block d-none" href="#" id="link-learning">Learning Center</a>
+              <Link href="/pricing" className="nav-link c-fs-3 c-fw-r d-lg-block d-none" id="link-pricing">Pricing</Link>
             </div>
             
             <Link className="navbar-brand m-auto" href={`${home}`}><img className=" d-lg-block d-none" src="/img/logo.svg" alt="MSG91" /></Link>
