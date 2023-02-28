92d5165187ea8ef0a81f84fd049dcc9a8f6d31fc
piyussssh <mvps1862@gmail.com>
2023-02-28 22:31:27 +0530

    case studies page

---
+++ pages/case-studies.js
@@ -0,0 +1,17 @@
+import Header from "@/components/header";
+import Footer from "@/components/footer-sec";
+import HeadTag from "@/components/head";
+import Casestudycard from "@/components/casestudy-card";
+const casestudy =()=>{
+    return(
+        <>
+        <HeadTag/>
+        <Header/>
+        <Casestudycard/>
+        <Footer/>
+        
+        </>
+
+    );
+};
+export default casestudy;
