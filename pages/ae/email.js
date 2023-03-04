be866233eda3f662313a739abcb1e04422eb567b
piyussssh <mvps1862@gmail.com>
2023-03-04 15:10:31 +0530

    ae/email bug fix

--- pages/ae/email.js
+++ pages/ae/email.js
@@ -1,12 +1,9 @@
 import Header from "@/components/header";
 import Notification from "@/components/notification";
 import HeadTag from "./head";
-import TrustedSec from "@/components/trusted_by";
-import Image from "next/image";
-import Link from "next/link";
 import Footer from "@/components/footer";
 import FaqSection from "@/components/faq";
-import Sms from "@/components/jsoncomponent/sms";
+import Email from "@/components/jsoncomponent/email";
 
 const Email = () => {
   return (
