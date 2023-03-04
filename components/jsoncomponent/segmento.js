49672b1b3717af252843f4d2605660eb9f069ac6
Anvesh800 <anvesh@whozzat.com>
2023-03-04 11:21:09 +0530

    segmento and whatsapp

--- components/jsoncomponent/segmento.js
+++ components/jsoncomponent/segmento.js
@@ -30,8 +30,7 @@ const segmento = () => {
           </div>
           <p className="c-fs-5 c-fw-sb text-uppercase col-campaign c-ls-20 mx-auto">STOP WONDERING, START SEGMENTING!</p>
           <h2 className="small-heading c-ff-b c-fw-r w-100 mx-auto">
-          Run targeted campaigns, personalize marketing content,
-precise data access with customer segmentation.{" "}
+          Run targeted campaigns, personalize marketing content, precise data access with customer segmentation.{" "}
           </h2>
           <button className="btn btn-primary btn-lg mt-3 c-fs-2" type="button">
             Get started for Free
@@ -91,10 +90,49 @@ Marketing Content
           </div>
         </div>
       </div>
-
- 
-
-
+      <div className="container">
+        <span className="sub-heading">More features</span>
+        <div className="container d-flex flex-column flex-md-row my-3">
+          <div className="d-flex flex-column justify-content-center align-items-flex-start col-lg-6 mt-2">
+            <span className="c-fs-3">
+              - {data?.segmento?.morefeatures?.one}
+            </span>
+            <span className="c-fs-3 mt-1">
+              - {data?.segmento?.morefeatures?.two}
+            </span>
+            <span className="c-fs-3 mt-1">
+              - {data?.segmento?.morefeatures?.three}
+            </span>
+            <span className="c-fs-3 mt-1">
+              - {data?.segmento?.morefeatures?.four}
+            </span>
+            <span className="c-fs-3 mt-1">
+              - {data?.segmento?.morefeatures?.five}
+            </span>
+            <span className="c-fs-3 mt-1">- {data?.segmento?.morefeatures?.six}</span>
+          </div>
+          <div className="my-auto mt-3 ms-4   px-5 d-flex py-5 c-bg-grey flex-wrap justify-content-center">
+            <span className="c-fs-2 c-ff-h">Looking for more?</span>
+            <button className="btn btn-outline-primary c-fs-4 ms-0 ms-md-3 mt-3 mt-md-0">
+              {" "}
+              Request a Feature
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-arrow-right ms-2"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
