e45cb25bfa1e58a94ee9981e85a2594397c3f7e1
piyussssh <mvps1862@gmail.com>
2023-03-14 14:35:10 +0530

    developers page

--- pages/developers.js
+++ pages/developers.js
@@ -1,146 +1,161 @@
 import HeadTag from "./head";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
-import { MdKeyboardArrowRight, MdDone } from "react-icons/md";
+import {GrAndroid, GrApple} from "react-icons/gr"
+import {
+  MdOutlinePeopleAlt,
+  MdOutlineChatBubbleOutline,
+  MdThumbUpOffAlt,
+} from "react-icons/md";
 
 const developers = () => {
   return (
     <>
       <HeadTag />
       <Header />
-      <div className="container mt-md-5 my-md-0 my-5 d-flex flex-column-reverse flex-md-row justify-content-between">
-        <div className="d-flex col-md-6 col-12 justify-content-center flex-column align-items-md-start me-md-5 me-0 mt-4 mt-md-0 text-center text-md-start align-items-center">
-          <h1 className="heading">MSG91 APIs for
-            Global messaging</h1>
-          <p className="c-fs-2 mt-3">
-            Integrate SMS, Email, Voice API with Java, PHP, HTTP Scripts. Build apps, innovation and ideas for rapid growing business, or just integrate for your team development.
-          </p>
-          <a href="https://msg91.com/startups-form/" className="btn btn-primary c-fs-4 mt-4">Explore documentation</a>
-        </div>
-        <div className="col-12 col-md-6">
-          <img className="startup-home-img flip-vertical" src="#" />
-        </div>
-      </div>
-      <div className="container text-center  overflow-hidden ">
+      <div className="container">
         <div className=" mx-auto text-center justify-content-center py-2 py-md-5 col-12 col-sm-8">
           <div className="d-flex justify-content-center align-items-center flex-column flex-sm-row">
-            <h2 className="heading">One API</h2>
+            <h1 className="heading">Hello Developers</h1>
           </div>
-
-          <p className="c-fs-1 c-fw-b mx-auto mb-3">
-            To revolutionalise the way developers integrate
+          <h2 className="c-fs-2 px-3 w-md-75 w-100 mx-auto">
+            Integrate SMS, Email, Voice API with Java, PHP, HTTP Scripts. Build
+            apps, innovation and ideas for rapid growing business, or just
+            integrate for your team development.
+          </h2>
+          <a href="#">
+            <button
+              className="btn btn-primary btn-lg mt-3 c-fs-4"
+              type="button"
+            >
+              Explore Documentation
+            </button>
+          </a>
+        </div>
+      </div>
+      <section className="col-12 c-bg-grey py-5">
+        <div className="container">
+          <h2 className="small-heading">
+            One API to revolutionalise the way developers integrate
+          </h2>
+          <p className="c-fs-3 mt-2">
+            A single, unified API to simplify communication across multiple
+            architectures. Build integrations faster and deliver a unified
+            experience across channels for faster performance, more productivity
+            and greater innovation.
           </p>
-          <p className="c-fs-2 mx-auto mb-3">
-            A single, unified API to simplify communication across multiple architectures. Build integrations faster and deliver a unified experience across channels for faster performance, more productivity and greater innovation.
-          </p>
-          <p className="c-fs-2 c-fw-b">
+          <p className="c-fs-3 c-fw-b mt-3">
+            {" "}
             Just integrate and forget the rest of the challenges.
           </p>
         </div>
-      </div>
-      <div className=" p-5 text-center justify-content-center">
-        <h3 className="sub-heading">
-          We enable communication in your awesome products
-        </h3>
-        <div className="d-flex flex-column flex-md-row gap-5 gap-md-0 my-5 align-items-center justify-content-center">
-          <div className="signup-cont c-bg-grey p-4 d-flex flex-column align-items-center justify-content-center">
-            <span className="c-fs-2 c-fw-sb">Engineered for reliability</span>
-            <p className="c-fs-4 mt-2">
-              Multi-route technology to deliver your
-              messages anywhere, anytime, anyhow!
-            </p>
-            <a href="/oneapi" className=" mt-1">Learn more</a>
-          </div>
-
-          <div className=" d-none p-5 c-fs-1 c-col-grey d-md-flex flex-column align-items-center justify-content-center">
-            <MdKeyboardArrowRight />
-          </div>
-          <div className="signup-cont c-bg-grey p-4 d-flex flex-column align-items-center justify-content-center">
-            <span className="c-fs-2 c-fw-sb">Communication addons</span>
-            <p className="c-fs-4 mt-2">
-              Configure the trendy plugins/addons
-              to ease out for what you want to build.
-            </p>
-            <a href="#" className=" mt-1">Learn more</a>
-          </div>
-
-          <div className=" p-5 c-fs-1 c-col-grey d-none d-md-flex flex-column align-items-center justify-content-center">
-            <MdKeyboardArrowRight />
-          </div>
-          <div className="signup-cont c-bg-grey p-4 d-flex flex-column align-items-center justify-content-center">
-            <span className="c-fs-2 c-fw-sb">Problem solvers at service</span>
-            <p className="c-fs-4 mt-2">
-              Contact our support superheroes 18*7 in case of any query, confusion or problem.
-            </p>
-            <a href="/contact-us" className=" mt-1">Talk to our experts</a>
-          </div>
-        </div>
-      </div>
-      <div className="container overflow-hidden ">
-        <div className=" mx-auto justify-content-center py-2 py-md-5 col-12 col-sm-8">
-          <div className="d-flex justify-content-center align-items-center flex-column flex-sm-row">
-            <h2 className="sub-heading">Why MSG91 API is developer friendly for real?</h2>
+      </section>
+      <div className="container my-5">
+        <h2>We enable communication in your awesome products</h2>
+        <div className="d-flex w-100">
+          <div className="col-12 col-lg-10  flex-column flex-md-row d-flex align-items-center my-4 justify-content-center gap-3 gap-lg-5 mx-auto">
+            <div className="developers-card">
+              <div className="w-100 d-flex flex-column align-items-center mt-4 mt-sm-0 mt-lg-4">
+                <span className="c-fs-1 c-fw-b  rounded-circle social-circle justify-content-center align-items-center d-flex">
+                  <MdThumbUpOffAlt className="p-auto text-primary" />
+                </span>
+                <p className="w-100 c-fs-2 px-2 py-5 mt-n3 rounded text-center c-bg-grey">
+                  Engineered for reliability
+                </p>
+              </div>
+            </div>
+            <div className="developers-card">
+              <div className="w-100 d-flex flex-column align-items-center mt-4 mt-sm-0 mt-lg-4">
+                <span className="c-fs-1 c-fw-b  rounded-circle social-circle justify-content-center align-items-center d-flex">
+                  <MdOutlineChatBubbleOutline className="p-auto text-primary" />
+                </span>
+                <p className="w-100 c-fs-2 px-2 py-5 mt-n3 rounded text-center c-bg-grey">
+                  Communication addons
+                </p>
+              </div>
+            </div>
+            <div className="developers-card">
+              <div className="w-100 d-flex flex-column align-items-center mt-4 mt-sm-0 mt-lg-4">
+                <span className="c-fs-1 c-fw-b  rounded-circle social-circle justify-content-center align-items-center d-flex">
+                  <MdOutlinePeopleAlt className="p-auto text-primary" />
+                </span>
+                <p className="w-100 c-fs-2 px-2 py-5 mt-n3 rounded text-center c-bg-grey">
+                  Problem solvers at service
+                </p>
+              </div>
+            </div>
           </div>
         </div>
-        <p className="c-fs-1 c-fw-b mx-auto mb-3">
-          Powerful sending infrastructure
-        </p>
-        <p className="c-fs-3 mx-auto mb-3">
-          Easy API integration and a simple, HTTP or JSON/XML API enables your App with Text, Voice and Email functionality. Scale quickly, whether it’s a matter of communicating to 10 users or 10 million.
-        </p>
-        <p className="c-fs-3 mx-auto mb-3">
-          Build exactly what you need with flexible <a href="/oneapi">APIs.</a>
-        </p>
-        <p className="c-fs-2 c-fw-b mb-3">
-          Tracking and analytics
-        </p>
-        <p className="c-fs-3 mx-auto mb-3">
-          Searchable logs means you always know what is happening to your messages, while tags make it easy to report on your data and all that via our efficient panel and webhooks.
-        </p>
-        <p className="c-fs-2 c-fw-b mb-3">
-          Manage your contact and SMS campaigns wisely
-        </p>
-        <p className="c-fs-3 mx-auto mb-3">
-          Track and optimize your SMS campaign and know how your customer is interacting with your messages.
-        </p>
       </div>
-      <div className="container mt-md-5 my-md-0 my-5 d-flex flex-column-reverse flex-md-row justify-content-between">
-        <div className="d-flex col-md-6 col-12 justify-content-center flex-column align-items-md-start me-md-5 me-0 mt-4 mt-md-0 text-center text-md-start align-items-center">
-          <h1 className="c-fs-1 c-fw-b">Start your custom code
-            adventure now</h1>
+      <section className="col-12 c-bg-grey py-5">
+        <div className="col-11 col-md-10 col-lg-9 mx-auto">
+          <h2 className="small-heading">
+            Why MSG91 API is developer friendly for real?
+          </h2>
+          <div className="mt-5">
+            <p className="c-fs-3 c-fw-b">Powerful sending infrastructure</p>
+            <p className="c-fs-3">
+              Easy API integration and a simple, HTTP or JSON/XML API enables
+              your App with Text, Voice and Email functionality. Scale quickly,
+              whether it’s a matter of communicating to 10 users or 10 million.
+              Build exactly what you need with flexible APIs.
+            </p>
+          </div>
+          <div className="mt-5">
+            <p className="c-fs-3 c-fw-b">Tracking and analytics</p>
+            <p className="c-fs-3">
+              Searchable logs means you always know what is happening to your
+              messages, while tags make it easy to report on your data and all
+              that via our efficient panel and webhooks.
+            </p>
+          </div>
+          <div className="mt-5">
+            <p className="c-fs-3 c-fw-b">
+              Manage your contact and SMS campaigns wisely
+            </p>
+            <p className="c-fs-3">
+              Track and optimise your SMS campaign and know how your customer is
+              interacting with your messages.
+            </p>
+          </div>
+        </div>
+      </section>
+      <div className="container d-flex flex-md-row  flex-column  justify-content-betweeen d-flex my-5 ">
+        <div className="col-12 col-md-6 pe-0 pe-md-4 mb-4 mb-md-0 d-flex flex-column align-content-center justify-content-center">
+          <h2 className="small-heading">
+            Start your custom code adventure now
+          </h2>
           <p className="c-fs-3">
-            Easily integrate MSG91 APIs for Sending SMS, OTP verification or communicate across channels. Access sample codes in Ruby, Python, PHP, Java and more.
+            Easily integrate MSG91 APIs for Sending SMS, OTP verification or
+            communicate across channels. Access sample codes in Ruby, Python,
+            PHP, Java and more.
           </p>
-          <a href="https://github.com/MSG91" className="c-fs-4 mt-2">Take me to MSG91 Github Repositories</a>
         </div>
-        <div className="col-12 col-md-6">
-          <img className="mx-auto" src="/img/developers.svg" />
+        <div className="col-md-6 col-12">
+          <img
+            className="developers-code-img w-100"
+            src="/img/developers-code-img.png"
+            alt="#"
+          />
         </div>
       </div>
-      <div className="container text-center overflow-hidden mt-3">
-        <div className=" mx-auto text-center justify-content-center py-2 py-md-5 col-12 col-sm-8">
-          <div className="d-flex justify-content-center align-items-center flex-column flex-sm-row">
-            <h2 className="heading">5 minute setup</h2>
+      <div className="container my-5 text-center d-flex flex-column align-items-center gap-4">
+        <h2 className="small-heading mt-5">5 Minute Setup</h2>
+        <p className="c-fs-3 col-12 col-md-9">Build real-time communications into your apps in a few minutes and a few lines of code.The SDKs seamlessly connect to MSG91 global infrastructure.</p>
+      <div className="col-4 justify-content-center gap-5 d-flex">
+        <div className="dev-page-ico ">
+          <div className="mx-auto c-bg-grey d-flex justify-content-center align-items-center">
+           <a href="https://play.google.com/store/apps/details?id=com.hellornapp"> <GrAndroid className="c-fs-1"/></a>
           </div>
-          <p className="c-fs-3 mx-auto mt-5">
-            Build real-time communications into your apps in a few minutes and a few lines of code.
-          </p>
-          <p className="c-fs-3 mx-auto mb-3">
-            The SDKs seamlessly connect to MSG91 global infrastructure.
-          </p>
-          <div className="d-flex text-center justify-content-center">
-            <div className="d-flex justify-content-center align-items-center flex-column flex-sm-row">
-              <a href="https://github.com/MSG91/sendotp-android" target="_blank">
-                <img src="/img/android_developers.svg" className="mt-5 me-5"></img>
-              </a>
-            </div>
-            <div className="d-flex justify-content-center align-items-center flex-column flex-sm-row">
-              <a href="https://github.com/MSG91/sendotp-ios" target="_blank">
-                <img src="/img/ios_developers.svg" className="mt-5 ms-5"></img>
-              </a>
-            </div>
-          </div>
+          <p className="c-fs-2">Android</p>
         </div>
+        <div className="dev-page-ico">
+          <div className="mx-auto c-bg-grey d-flex justify-content-center align-items-center">
+           <a href="https://apps.apple.com/in/app/msg91/id981827687"> <GrApple className="c-fs-1"/></a>
+          </div>
+          <p className="c-fs-2">iOS</p>
+        </div>
+      </div>
       </div>
       <Footer />
     </>
